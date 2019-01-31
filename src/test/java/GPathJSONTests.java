8a9eb0d8465f96a5f28bf71a4001fc0d6a8d9a3f
Vitaliy Ismagilov <ismagilov.vel@gmail.com>
2019-01-31 17:17:23 +0200

    30. Groovy GPath Part 2 - 'find' and 'findAll'

--- src/test/java/GPathJSONTests.java
+++ src/test/java/GPathJSONTests.java
@@ -2,6 +2,7 @@ import config.TestConfig;
 import io.restassured.response.Response;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
@@ -18,6 +19,19 @@ public class GPathJSONTests extends TestConfig {
     }
 
 
+    @Test
+    public void extractSingleValueWithFind() {
+        Response response = given().get("teams/18");
+        String certainPlayer = response.path("squad.find {it.shirtNumber == 35}.name");
+        System.out.println(certainPlayer);
+    }
 
+
+    @Test
+    public void extractListOfValuesWithFindAll() {
+        Response response = given().get("teams/18");
+        List<?> values = response.path("squad.findAll {it.shirtNumber < 25}.name"); // List<?> or <String>
+        System.out.println(values);
+    }
 
 }
