f49d41a6e810178ba22fd1785504cb74a13205fc
Vitaliy Ismagilov <ismagilov.vel@gmail.com>
2019-02-01 12:15:01 +0200

    36. Groovy GPath XML Part 3 - Extract all XML nodes with 'findAll'

--- src/test/java/GPathXMLTests.java
+++ src/test/java/GPathXMLTests.java
@@ -1,7 +1,11 @@
 import config.EndPoint;
 import config.TestConfig;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.path.xml.element.Node;
 import io.restassured.response.Response;
 import org.junit.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 
@@ -23,7 +27,16 @@ public class GPathXMLTests extends TestConfig {
 
        String category = response.path("videoGames.videoGame[0].@category");
        System.out.println(category);
+   }
 
+
+   @Test
+    public void getListOfXmlNodes() {
+       String responseAsString = get(EndPoint.VIDEOGAMES).asString();
+
+       List<Node> allResults = XmlPath.from(responseAsString).get(
+               "videoGames.videoGame.findAll {element -> return element}");
+       System.out.println(allResults.get(2).get("name").toString());
    }
 
 }
