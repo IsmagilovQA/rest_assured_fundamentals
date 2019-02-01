c69b45886f8cec932fb87185ca13b6e2fff54c20
Vitaliy Ismagilov <ismagilov.vel@gmail.com>
2019-02-01 12:26:59 +0200

    37. Groovy GPath XML Part 4 - Extract XML nodes with an attribute

--- src/test/java/GPathXMLTests.java
+++ src/test/java/GPathXMLTests.java
@@ -39,4 +39,18 @@ public class GPathXMLTests extends TestConfig {
        System.out.println(allResults.get(2).get("name").toString());
    }
 
+
+   @Test
+    public void getListOfXmlNodesByFindAllOnAttribute() {
+       String responseAsString = get(EndPoint.VIDEOGAMES).asString();
+
+       List<Node> allDrivingGames = XmlPath.from(responseAsString).get(
+               "videoGames.videoGame.findAll " +
+                       "{videoGame -> def category = videoGame.@category; category == 'Driving'}");
+       System.out.println(allDrivingGames.get(0).get("name").toString());
+
+
+
+   }
+
 }
