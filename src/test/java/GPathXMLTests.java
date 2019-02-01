93550706e574f94b5de86216f963a25a9066fe54
Vitaliy Ismagilov <ismagilov.vel@gmail.com>
2019-02-01 12:51:58 +0200

    39. Groovy GPath XML Part 6 - Depth First Search

--- src/test/java/GPathXMLTests.java
+++ src/test/java/GPathXMLTests.java
@@ -7,59 +7,68 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.get;
 
 
 public class GPathXMLTests extends TestConfig {
 
-   @Test
-   public void getFirstGameInList() {
-       Response response = get(EndPoint.VIDEOGAMES);
+    @Test
+    public void getFirstGameInList() {
+        Response response = get(EndPoint.VIDEOGAMES);
 
-       String firstName = response.path("videoGames.videoGame.name[0]");
-       System.out.println(firstName);
-   }
+        String firstName = response.path("videoGames.videoGame.name[0]");
+        System.out.println(firstName);
+    }
 
 
-   @Test
+    @Test
     public void getAttributeName() {
-       Response response = get(EndPoint.VIDEOGAMES);
+        Response response = get(EndPoint.VIDEOGAMES);
 
-       String category = response.path("videoGames.videoGame[0].@category");
-       System.out.println(category);
-   }
+        String category = response.path("videoGames.videoGame[0].@category");
+        System.out.println(category);
+    }
 
 
-   @Test
+    @Test
     public void getListOfXmlNodes() {
-       String responseAsString = get(EndPoint.VIDEOGAMES).asString();
+        String responseAsString = get(EndPoint.VIDEOGAMES).asString();
 
-       List<Node> allResults = XmlPath.from(responseAsString).get(
-               "videoGames.videoGame.findAll {element -> return element}");
-       System.out.println(allResults.get(2).get("name").toString());
-   }
+        List<Node> allResults = XmlPath.from(responseAsString).get(
+                "videoGames.videoGame.findAll {element -> return element}");
+        System.out.println(allResults.get(2).get("name").toString());
+    }
 
 
-   @Test
+    @Test
     public void getListOfXmlNodesByFindAllOnAttribute() {
-       String responseAsString = get(EndPoint.VIDEOGAMES).asString();
+        String responseAsString = get(EndPoint.VIDEOGAMES).asString();
 
-       List<Node> allDrivingGames = XmlPath.from(responseAsString).get(
-               "videoGames.videoGame.findAll " +
-                       "{videoGame -> def category = videoGame.@category; category == 'Driving'}");
-       System.out.println(allDrivingGames.get(0).get("name").toString());
-   }
+        List<Node> allDrivingGames = XmlPath.from(responseAsString).get(
+                "videoGames.videoGame.findAll " +
+                        "{videoGame -> def category = videoGame.@category; category == 'Driving'}");
+        System.out.println(allDrivingGames.get(0).get("name").toString());
+    }
 
 
-   @Test
+    @Test
     public void getSingleNode() {
-       String responseAsString = get(EndPoint.VIDEOGAMES).asString();
+        String responseAsString = get(EndPoint.VIDEOGAMES).asString();
 
-       Node videoGame = XmlPath.from(responseAsString).get(
-               "videoGames.videoGame.find {videoGame -> def name = videoGame.name; name == 'SimCity 2000'}");
-       String videoGameName = videoGame.get("name").toString();
-       System.out.println(videoGameName);
+        Node videoGame = XmlPath.from(responseAsString).get(
+                "videoGames.videoGame.find {videoGame -> def name = videoGame.name; name == 'SimCity 2000'}");
+        String videoGameName = videoGame.get("name").toString();
+        System.out.println(videoGameName);
+    }
 
-   }
+
+    @Test
+    public void getSingleElementDepthFirst() {
+        String responseAsString = get(EndPoint.VIDEOGAMES).asString();
+
+        int reviewScore = XmlPath.from(responseAsString).getInt(
+                "**.find {it.name == 'SimCity 2000'}.reviewScore");
+        System.out.println(reviewScore);
+    }
 
 }
